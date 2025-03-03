9e1ea5fd932b522af42bdc62f57caad4c854380a
Michal Maruska <mmaruska@gmail.com>
2025-03-03 10:35:43 +0100

    test: try ot benchmar, example, fuzz

--- topologicalSort/topologicalSort_test.go
+++ topologicalSort/topologicalSort_test.go
@@ -8,7 +8,7 @@ import (
 	"fmt"
 )
 
-func TestSort(t *testing.T) {
+func TestTopoSort(t *testing.T) {
 	// Example graph (adjacency list representation)
 	// Number of nodes
 	// if testing.Short() {
@@ -43,4 +43,41 @@ func TestSort(t *testing.T) {
 	}
 }
 
+func BenchmarkToposort(b *testing.B) {
+	n := 6
+
+	graph := NewGraph(n)
+	graph.AddEdge(5, 2)
+	graph.AddEdge(5, 0)
+	graph.AddEdge(4, 0)
+	graph.AddEdge(4, 1)
+	graph.AddEdge(2, 3)
+	graph.AddEdge(3, 1)
+
+	b.ResetTimer()
+	for range b.N  { // b.Loop()
+	// Call topologicalSort
+	result, err := Sort(graph)
+	if err != nil {
+		// t.Errorf("Error: %s", err)
+	} else {
+		// assert_equal()
+		if testing.Verbose() {
+			fmt.Println("Topological Sort Order:", result)
+		}
+	}
+    }
+}
+
 // func TestMain(m *testing.M){}
+
+/// Examples
+func ExampleHello() {
+    fmt.Println("hello")
+    // Output: hello
+}
+
+
+// Fuzzing
+// randomly generated inputs
+//  f.Fuzz(func(t *testing.T, in []byte) {
