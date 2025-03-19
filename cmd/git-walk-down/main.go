b7134f4f3cb7f77ecff4cf67878fa63519ff9b7a
Michal Maruska <mmaruska@gmail.com>
2025-03-19 05:50:06 +0100

    git-walk-down: clone option + cloneHierarchy()

--- cmd/git-walk-down/main.go
+++ cmd/git-walk-down/main.go
@@ -12,6 +12,92 @@ import (
 	"github.com/pborman/getopt/v2"
 )
 
+
+
+func cloneHierarchy(vertices *[]graph.NodeExpander, order []int, prefix string) {
+	// assignment to entry in nil map
+	var renamed  = make(map[string]*plumbing.Reference) // Name
+	// so what's the difference between this & make() ?
+
+	// can I simplify this iterator?
+	for i := range order {
+		gh := git_hierarchy.GetHierarchy((*vertices)[i])
+
+		fmt.Println("cloning", gh.Name() )
+		// skip over?
+
+		//
+		switch gh.(type) {
+		case git_hierarchy.Segment:
+			segment := gh.(git_hierarchy.Segment)
+
+			name := segment.Name()
+			// ignore?
+			newName :=prefix + name
+
+			// newRef := plumbing.ReferenceName(git_hierarchy.HeadPrefix + newName)
+			fmt.Println("new segment ", newName)
+
+			base := segment.Base
+			if val, found := renamed[base.Name().String()]; found {
+				// so what is
+				base = val
+			}
+
+			newSegment := git_hierarchy.MakeSegment(
+				newName, base,
+				segment.Ref.Hash(),
+				segment.Start.Hash())
+			/*
+			hash := segment.Ref.Hash()
+			newSegment := git_hierarchy.Segment{
+				Ref: plumbing.NewHashReference(newName, hash),
+				Base: segmentBase(newName),
+				Start: segment.Start}
+			*/
+			fmt.Println("Segment", newSegment.Ref.Name(),
+				"base", newSegment.Base.Name(),
+				"->", newSegment.Base.Target(),
+				"start", newSegment.Start.Name(), "->", hash)
+			newSegment.Write()
+
+			// how to create it?
+			renamed[name] = newSegment.Ref
+
+		case git_hierarchy.Sum:
+			sum := gh.(git_hierarchy.Sum)
+			name := sum.Name()
+			newName := plumbing.ReferenceName(prefix + name)
+
+			hash := sum.Ref.Hash()
+			newSum := git_hierarchy.Sum{
+				Ref: plumbing.NewHashReference(newName, hash),
+				Summands: make([]*plumbing.Reference, len(sum.Summands)) }
+
+			// list-of new summands.
+
+			// create sum, with summands....
+			for i, summand := range sum.Summands {
+				newTarget := summand.Target()
+				// points into renamed[]
+				// then rewrite.
+
+				if val, found := renamed[newTarget.String()]; found {
+					newTarget = val.Name()
+				}
+				newSum.Summands[i] = plumbing.NewSymbolicReference(
+					summand.Name(),
+					newTarget)
+			}
+			// for summand ... renamed ?
+
+		default:
+		}
+	}
+}
+
+
+
 //  mixed named and unnamed parameters
 func replaceInHierarchy(vertices *[]graph.NodeExpander, order []int, from string, replacement string) {
 	// todo: replaceFlag must be a ReferenceName -- existing!
@@ -75,6 +161,7 @@ func main() {
 	skipOpt := set.StringLong("skip", 's', "", "skip")
 	replaceFlag := set.StringLong("replace", 't', "", "replace")
 
+	cloneOpt := set.StringLong("clone", 'c', "", "clone using a prefix")
 
 
 
@@ -144,6 +231,9 @@ func main() {
 		replaceInHierarchy(vertices, order, *skipOpt, *replaceFlag)
 	}
 
+	if *cloneOpt != "" {
+		cloneHierarchy(vertices, order, *cloneOpt)
+	}
 
 	// dump index -> vertices[i]
 	// fmt.Println("order:", order)
