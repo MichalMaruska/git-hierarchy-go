c400379220882c4b5ab06f0382e535d32cc3c4c5
Michal Maruska <mmaruska@gmail.com>
2025-03-22 11:00:53 +0100

    usage: by Parse() only!

--- cmd/git-walk-down/main.go
+++ cmd/git-walk-down/main.go
@@ -188,6 +188,21 @@ func findGitRepository() (*git.Repository, error){
 	return git.PlainOpenWithOptions(".", &openOptions)
 }
 
+/*
+   PrintUsage
+ */
+
+func usage(set *getopt.Set){
+	writer := os.Stderr
+	// set.PrintUsage(writer)
+	fmt.Fprintln(writer, set.Program(), set.UsageLine())
+	set.PrintOptions(writer)
+	// [-h] [-c value] [-s value] [-t value]
+
+	fmt.Fprintln(writer, "\nparameter:  from  to")
+	fmt.Fprintln(writer, "Example\n -s refs/heads/debian-unstable -t refs/remotes/debian/debian-unstable mmc-fork ")
+}
+
 func main() {
 	// os.Args[0] == "git-walk-down"
 	set := getopt.New()
@@ -200,10 +215,10 @@ func main() {
 	cloneOpt := set.StringLong("clone", 'c', "", "clone using a prefix")
 
 	// no errors, just fail:
-	set.SetUsage(func() {
-		getopt.PrintUsage(os.Stderr)
-		fmt.Println("\nparameter:  from  to")
-	})
+	// I don't use Parse(), but Getopt() so!!!
+	set.SetUsage(func() {usage(set)}) // to display usage on error.
+
+	set.SetParameters("[top-reference]")
 
 	// var opts = getopt.CommandLine
 	var current = ""
@@ -240,11 +255,13 @@ func main() {
 		}
 		}
 		return true }); err != nil {
+
+		// usage(set)
+		fmt.Fprintln(os.Stderr, "received error")
 		fmt.Fprintln(os.Stderr, err)
 		set.PrintUsage(os.Stderr)
 		os.Exit(1)
 	}
-
 
 	// sanity check:
 	if (*replaceFlag != "") != (*skipOpt != "") {
