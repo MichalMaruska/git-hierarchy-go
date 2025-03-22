0d48fe52c5c6afa8d4f5f25a61c78aa18d46310b
Michal Maruska <mmaruska@gmail.com>
2025-03-22 12:24:33 +0100

    build with shadowing detection

--- build.sh
+++ build.sh
@@ -3,7 +3,18 @@
 set -x
 go vet ./...
 
+go vet -vettool=/home/michal/go/bin/shadow ./...
+# I built it myself:
+# /home/michal/go/pkg/mod/golang.org/x/tools@v0.31.0/README.md
+
+# go install golang.org/x/tools/cmd/goimports@latest
+# go build /home/michal/go/pkg/mod/golang.org/x/tools@v0.31.0/go/analysis/passes/shadow/cmd/shadow/main.go
+# mv main ~/go/bin/shadow
+
+# /home/michal/go/pkg/mod/golang.org/x/tools@v0.31.0/go/analysis/passes/shadow/cmd/shadow ./...
+
 ~/go/bin/ineffassign ./...
 
 # -v print the names of packages as they are compiled
 go install -v ./...
+
